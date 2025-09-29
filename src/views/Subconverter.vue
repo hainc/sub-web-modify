b4c42bb936126f0d75c8fb6cbc4e23685b3137ca
辉哥 <m@haah.net>
2025-09-29 17:58:00 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,7 +339,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "辉哥自用": "https://subapi.haah.net",
+          "辉哥自用": "https://sub-api.cdn.haah.net",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -349,7 +349,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "https://subapi.haah.net" },
+          { value: "https://sub-api.cdn.haah.net" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -802,7 +802,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://subapi.haah.net" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://sub-api.cdn.haah.net" : this.getUrlParam(),
         shortType: "https://subs.cdn.haah.net",
         remoteConfig: "https://raw-github.cdn.haah.net/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
@@ -1326,5 +1326,6 @@ export default {
   }
 };
 </script>
+
 
 
